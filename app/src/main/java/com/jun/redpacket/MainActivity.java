f19db56f5135ce429855e523667c28a22eecffb3
LittleRedArmy <mr.ajun@foxmail.com>
2017-01-09 13:31:01 +0800

    no message

--- app/src/main/java/com/jun/redpacket/MainActivity.java
+++ app/src/main/java/com/jun/redpacket/MainActivity.java
@@ -3,6 +3,15 @@ package com.jun.redpacket;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * @Company: ****科技有限公司
+ * @Class: MainActivity
+ * @Description:
+ * @Author: HSJ
+ * @Version: RedPacket V1.0
+ * @Date: 2017/1/9 11:35
+ * @E-mail: mr.ajun@foxmail.com
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
